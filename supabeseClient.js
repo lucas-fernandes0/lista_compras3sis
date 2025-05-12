ebf64f5dfedee78381b489d45eb6cff06ee10a8b
lucas-fernandes0 <160758771+lucas-fernandes0@users.noreply.github.com>
2025-05-12 12:33:20 -0300

    .

--- supabeseClient.js
+++ supabeseClient.js
@@ -4,5 +4,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
   'https://daezjswznstiyapohwna.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhZXpqc3d6bnN0aXlhcG9od25hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU1NTYsImV4cCI6MjA2MjExMTU1Nn0.W6ds4DAC_TXXWFfjhIKy9hAP_7AleM21NNen6q5uJsc'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhZXpqc3d6bnN0aXlhcG9od25hIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NjUzNTU1NiwiZXhwIjoyMDYyMTExNTU2fQ.xsPMIFaBJ2_dZbGfkPac_AM1iZyJS9-D4rImsXnsoLM'
 )
